87b34fd08c3b289d94c6da64c1560999868337de
Martin Hanzel <arthanzel@gmail.com>
2018-01-30 16:37:48 -0500

    Fixed locals handling typo

--- client/src/evaluatex/evaluatex.js
+++ client/src/evaluatex/evaluatex.js
@@ -3,8 +3,8 @@ import Parser from "./parser";
 import * as localFunctions from "./util/localFunctions";
 
 export default function evaluatex(expression, locals = {}) {
-    let l = new Lexer(expression);
-    let p = new Parser(l.tokens, locals);
+    let l = new Lexer(expression, locals);
+    let p = new Parser(l.tokens);
     let ast = p.parse();
     return function(_locals = {}) { ast.evaluate(_locals); }
 }
