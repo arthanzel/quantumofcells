aa63066c73ca58038fab4e6fe522e03ae7eef42e
Martin Hanzel <arthanzel@gmail.com>
2018-02-06 14:18:18 -0500

    Moved editor root CSS to its own file

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from "react-dom";
 
 import QOCApplication from "components/QOCApplication";
 
-ReactDOM.render(<QOCApplication />, document.getElementById("qoc-app-container"));
+import "index.styl"
 
-// class Equation extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     render() {
-//         return <div>
-//             Equation { this.props.body }
-//         </div>;
-//     }
-// }
-//
-// ReactDOM.render(<Equation body="2+4" />, document.getElementById("react-root"));
+ReactDOM.render(<QOCApplication />, document.getElementById("qoc-app-container"));
