2334dc6489715d9022e9d89e7af1dfe9ab9540b4
Martin Hanzel <arthanzel@gmail.com>
2018-02-13 15:25:17 -0500

    Started to integrate auth0

--- client/src/components/LoginBox/index.js
+++ client/src/components/LoginBox/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { WebAuth } from "auth0-js";
 
 import "./LoginBox.styl";
 
 export default class LoginBox extends React.Component {
+    login() {
+        let webAuth = new WebAuth({
+            domain: "qoc.auth0.com",
+            clientID: "IIlRrl3lLjVaHqJezrqVN3C3YoM1x5Fw",
+            redirectUri: window.location.href
+        });
+        console.log("webauth", webAuth);
+        webAuth.authorize({
+            responseType: "token id_token"
+        });
+    }
+
     render() {
         return <footer>
             Hello, Martin.&nbsp;
-            <a href="#">Not you?</a>
+            <a href="#" onClick={this.login}>Not you?</a>
         </footer>
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -8,4 +8,16 @@ import "index.styl"
 // Create the Redux store
 import "qoc/store";
 
+import { WebAuth } from "auth0-js";
+
 ReactDOM.render(<QOCApplication />, document.getElementById("qoc-app-container"));
+
+let webAuth = new WebAuth({
+    domain: "qoc.auth0.com",
+    clientID: "IIlRrl3lLjVaHqJezrqVN3C3YoM1x5Fw",
+    redirectUri: window.location.href
+});
+webAuth.parseHash((err, result) => {
+    console.log("error", err);
+    console.log(result);
+});
