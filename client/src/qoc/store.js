c8437894538daa7238ac4ff8498c47baedbf56a0
Martin Hanzel <arthanzel@gmail.com>
2018-02-13 20:36:30 -0500

    Enabled redux dev tools

--- client/src/qoc/store.js
+++ client/src/qoc/store.js
@@ -29,10 +29,14 @@ let initialState = {
 };
 initialState = Object.assign(initialState, examples.sir);
 
-const store = createStore(rootReducer, initialState);
+const store = createStore(rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
 
 // Observable-style function to subscribe to state changes
 export const listen = function(listener) {
-    return store.subscribe(() => { listener(store.getState()); });
+    return store.subscribe(() => {
+        listener(store.getState());
+    });
 };
