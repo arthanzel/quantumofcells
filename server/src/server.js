831c419d9c93434ceddde43a246fe133d8003330
Martin Hanzel <arthanzel@gmail.com>
2018-02-16 19:29:12 -0500

    Testing a secure endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,39 @@
 import express from "express";
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 
 const app = express();
-const PORT = process.env.PORT || 8081;
 
-app.get("/", (req, res) => {
-    res.send("Hello!");
+const checkJwt = jwt({
+    audience: process.env.AUTH_AUDIENCE,
+    issuer: process.env.AUTH_ISSUER,
+    algorithms: ["RSA256"],
+
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: process.env.AUTH_JWKS_URI
+    })
 });
 
-app.listen(PORT);
-console.log(`Quantum of Cells API listening on ${PORT}`);
+app.get("/test", (req, res) => {
+    res.send("Hello!");
+});
+app.get("/test/auth", checkJwt, (req, res) => {
+    res.send("Authenticated!");
+});
+
+app.use((err, req, res, next) => {
+    switch (err.status) {
+        case 401:
+            res.status(401).json({ error: "401 Not Authorized" });
+            break;
+        default:
+            res.status(500).json({ error: "500 Server Error" });
+            break;
+    }
+});
+
+app.listen(process.env.PORT);
+console.log(`Listening on ${process.env.PORT}`);
