76896347bad504747e4ff870c09b8b8e307610e2
Martin Hanzel <arthanzel@gmail.com>
2018-02-22 19:46:42 -0500

    Added Storybook story for EquationBox

--- client/.storybook/webpack.config.js
+++ client/.storybook/webpack.config.js
@@ -6,13 +6,15 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
-    ],
-  },
-};
+module.exports = require("../webpack.config");
+
+// module.exports = {
+//   plugins: [
+//     // your custom plugins
+//   ],
+//   module: {
+//     rules: [
+//       // add your custom rules.
+//     ],
+//   },
+// };
