fda966ebd12257f5652b6ea221040a425f48dcd4
Martin Hanzel <arthanzel@gmail.com>
2018-02-24 16:21:55 -0500

    Fixed folder issue with deploy

--- client/deploy.sh
+++ client/deploy.sh
@@ -2,10 +2,11 @@ branch=$(git symbolic-ref --short HEAD)
 root=$(git rev-parse --show-toplevel)
 cwd=$(pwd)
 
-cd "$root"
+cd "$root/client"
 git checkout gh-pages
 git merge master
 npm run build
+cd ..
 git subtree push --prefix client origin gh-pages
 git checkout "$branch"
 cd "$cwd"
