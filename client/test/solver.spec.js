7dcafec5eba8a490d4546300e6a643448dba140d
Martin Hanzel <arthanzel@gmail.com>
2018-02-24 21:16:40 -0500

    Removed an unfinished test case

--- client/test/solver.spec.js
+++ client/test/solver.spec.js
@@ -4,11 +4,8 @@ import { describe, it } from "mocha"
 import solver from "qoc/solver";
 
 describe("QOC Solver", function() {
-    it("should solve differential equations", function() {
+    xit("should solve differential equations", function() {
         let eqn = "4";
         let data = solver(eqn, { x: 0 }, 2, 100);
-        for (let i of data.data["0"]) {
-            //console.log(i);
-        }
     });
 });
