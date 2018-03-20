98fec8391ad4afb16b1c8738d5b0f976ddae74ab
Martin Hanzel <arthanzel@gmail.com>
2018-03-20 13:09:23 -0400

    Styled new project form with Bootstrap

--- client/src/components/CreateProjectForm.js
+++ client/src/components/CreateProjectForm.js
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 
 export default class CreateProjectForm extends React.Component {
     static defaultProps = {
-        onSubmit: () => {}
+        onSubmit: () => {
+        }
     };
 
     static propTypes = {
@@ -27,8 +28,11 @@ export default class CreateProjectForm extends React.Component {
     render() {
         return <form onSubmit={this.handleSubmit}>
             <h2>Create a Project</h2>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            <div>
+            <div className="form-group">
+                <input id="name" className="form-control" placeholder="Name" type="text" value={this.state.value} onChange={this.handleChange} />
+                <small class="form-text text-muted">Choose wisely, because the name can't be changed later.</small>
+            </div>
+            <div className="form-group">
                 <input type="submit" className="btn btn-primary" value="Create" />
             </div>
         </form>
