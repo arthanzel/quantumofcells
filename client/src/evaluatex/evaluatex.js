71a93cb96a93f204e9532950ca6978ce9f8d6226
Martin Hanzel <arthanzel@gmail.com>
2018-03-21 08:50:46 -0400

    Removed redundant new statement

--- client/src/evaluatex/evaluatex.js
+++ client/src/evaluatex/evaluatex.js
@@ -2,8 +2,8 @@ import lexer from "./lexer";
 import parser from "./parser";
 
 export default function evaluatex(expression, locals = {}) {
-    const tokens = new lexer(expression, locals);
-    const ast = new parser(tokens).simplify();
+    const tokens = lexer(expression, locals);
+    const ast = parser(tokens).simplify();
     const fn = function(_locals = {}) { return ast.evaluate(_locals); };
     fn.ast = ast;
     fn.expression = expression;
