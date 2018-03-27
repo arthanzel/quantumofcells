3c43890979f5967c0c2264899d58256527d3efb3
Martin Hanzel <arthanzel@gmail.com>
2018-03-27 19:26:24 -0400

    Updated footer

--- client/src/landing.js
+++ client/src/landing.js
@@ -1,4 +1,7 @@
+import { version } from "../package";
 import { webAuth } from "./qoc/util/authUtils";
+
+document.getElementById("version").innerText = version;
 
 window.login = function() {
     webAuth.authorize();
